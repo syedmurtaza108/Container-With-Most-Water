5cbd11665b0c56211e1d910ba8ca8e9c0a180764
syedmurtaza108 <murtaza.hussain@koderlabs.com>
2021-10-25 14:26:51 +0500

    problem solved

--- Container With Most Water/Program.cs
+++ Container With Most Water/Program.cs
@@ -6,7 +6,25 @@ namespace Container_With_Most_Water
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(MaxArea(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }));
+            //Console.WriteLine(MaxArea(new int[] { 2, 3, 4, 5, 18, 17, 6 }));
+        }
+
+        static int MaxArea(int[] height)
+        {
+            int maxArea = int.MinValue;
+            for (int i = 0, j = height.Length - 1; i < height.Length;)
+            {
+                int _base = j - i;
+                int _height = Math.Min(height[i], height[j]);
+                int area = _base * _height;
+                maxArea = Math.Max(area, maxArea);
+                if (height[i] > height[j]) j--;
+                else i++;
+                if (j < i) break;
+            }
+
+            return maxArea;
         }
     }
 }
